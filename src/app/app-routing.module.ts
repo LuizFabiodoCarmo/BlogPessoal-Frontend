07322730edd8cd08ab71ee2644e2678075f36955
LuizFabiodoCarmo <104438927+LuizFabiodoCarmo@users.noreply.github.com>
2022-07-08 12:52:46 -0300

    Criando rotas para Login e Cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [ //rai de objetos de rotas 
+{path:'', redirectTo: 'login', pathMatch: 'full'}, //criamos um path vázio para que seja redirecionada para 'login'
+{path:'login', component: LoginComponent},  
+{path: 'cadastrar', component: CadastrarComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
